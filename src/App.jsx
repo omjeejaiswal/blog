f994ca3587fa108a1dfca6e83494649dfb5a0c6c
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-10-13 23:56:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,60 @@ import {login, logout} from "./store/authSlice"
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
+// function App() {
+//   const [loading, setLoading] = useState(true)
+//   const dispatch = useDispatch()
+
+//   useEffect(() => {
+//     authService.getCurrentUser()
+//       .then((userData) => {
+//           if(userData) {
+//             dispatch(login({userData}))
+//           } else{
+//             dispatch(logout())
+//           }
+//       })
+//       .finally(() => setLoading(false))
+//   },[])
+
 function App() {
-  const [loading, setLoading] = useState(true)
-  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    authService.getCurrentUser()
-      .then((userData) => {
-          if(userData) {
-            dispatch(login({userData}))
-          } else{
-            dispatch(logout())
-          }
-      })
-      .finally(() => setLoading(false))
-  },[])
-  
-  
- return !loading ? (
-  <div className='min-h-screen-flex flex-wrap content-between bg-gray-400'> 
-    <div className='w-full block'>
-      <Header />
-      <main>
-          {/* outlet */}
-      </main>
-      <Footer />
+    const checkAuthStatus = async () => {
+      try {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      } catch (error) {
+        console.error("Error during authentication:", error);
+        dispatch(logout()); // Logout if an error occurs
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    checkAuthStatus();
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'> 
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* Router outlet or dynamic content */}
+        </main>
+        <Footer />
+      </div>
     </div>
-  </div>
- ) : null
-
+  ) : null;
 }
 
+
 export default App
+
+
